3a81a5e6de8e3cc526df413f93e1ac7ec1400a6f
diyews <diyews@users.noreply.github.com>
2019-06-28 17:44:14 +0800

    refactor: m3u8Merger param refactor

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,15 @@ import { downloadM3u8 } from './download-m3u8';
 import { downloadTs } from './download-ts';
 import { join } from 'path';
 
-export async function m3u8Merger({url, dir, isConvert} = {
-  dir: './ts',
-  isConvert: null,
-  url: '',
+export async function m3u8Merger({url, dir, isConvert}: {
+  dir: string,
+  isConvert?: null,
+  url: string,
 }) {
   if (!url) {
     return console.error('Muse provide a url');
   }
+  dir = dir || './ts';
 
   const urlPrefix = url.match(/(.+\/).+\.m3u8/)[1];
   const storageDir = join(process.cwd(), dir);
